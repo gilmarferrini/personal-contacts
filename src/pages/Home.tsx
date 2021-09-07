f258a388b5b4a05521cbd19cf064cccc9c67ad50
gilmarferrini <ferrinigilmar@gmail.com>
2021-09-07 17:03:38 -0300

    add flatlist

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FlatList } from 'react-native'
 
-import { MyContactCard, Button } from '../components/'
+import { MyContactCard, Button } from '../components'
 
 import { 
   Container,
@@ -10,7 +11,34 @@ import {
   Line
 } from './styles'
 
+interface Contact {
+  id: string
+  name: string
+  email: string
+  phone: string
+}
+
 export function Home() {
+
+  const [ contacts, setContacts ] = useState<Contact[]>([])
+  const [ name, setName ] = useState('')
+  const [ email, setEmail ] = useState('')
+  const [ phone, setPhone ] = useState('')
+
+  function handleAddNewContact() {
+    const id = new Date().getTime().toString()
+    const contact = {
+      id,
+      name,
+      email,
+      phone
+    }
+
+    console.log(contact)
+
+    setContacts([...contacts, contact])
+  }
+
   return (
     <>
       <Container>
@@ -29,52 +57,45 @@ export function Home() {
 
         <Input 
           placeholder="Entre com o nome"
+          value={name}
+          onChangeText={value => setName(value)}
         />
 
         <Input 
           placeholder="Entre com o email"
           keyboardType="email-address"
+          value={email}
+          onChangeText={value => setEmail(value)}
         />
 
         <Input 
           placeholder="Entre com o telefone"
           keyboardType="numeric"
+          value={phone}
+          onChangeText={value => setPhone(value)}
         />
 
         <Button 
           title="Adicionar"
+          onPress={handleAddNewContact}
         />
 
-        <Line />
+        <FlatList 
+          showsVerticalScrollIndicator={false}
+          data={contacts}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <>
+              <Line />
 
-        <MyContactCard 
-          name="Gilmar"
-          email="ferrinigilmar@gmail.com"
-          phone="998375424"
-        />
+              <MyContactCard 
+                name={item.name}
+                email={item.email}
+                phone={item.phone}
+              />
+            </>
+          )}
 
-        <Line />
-
-        <MyContactCard 
-          name="Gilmar"
-          email="ferrinigilmar@gmail.com"
-          phone="998375424"
-        />
-
-        <Line />
-
-         <MyContactCard 
-          name="Gilmar"
-          email="ferrinigilmar@gmail.com"
-          phone="998375424"
-        />
-
-        <Line />
-
-        <MyContactCard 
-          name="Gilmar"
-          email="ferrinigilmar@gmail.com"
-          phone="998375424"
         />
 
         <Line />
